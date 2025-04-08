6db17a0ff7d1d0f0199eaa99497d094bf8b7b707
Matt Castoro <matthewjcastoro@gmail.com>
2025-04-08 16:55:41 -0600

    Create driver script and move prettyPrint
    Create driver script for testing purposes.  Move prettyPrint to its
    own file and import to driver script.

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -28,20 +28,4 @@ class Tree {
 
 }
 
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-  if (node === null) {
-    return;
-  }
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-  }
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-  }
-};
-
-
-const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-// console.log(tree.root);
-prettyPrint(tree.root);
+export { Tree };
---
+++ driver_script.js
@@ -0,0 +1,6 @@
+import { prettyPrint } from "./prettyPrint.js";
+import { Tree } from "./binary_search_tree.js";
+
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+prettyPrint(tree.root);
